fc3af8c4014897b3e5ef0248ce58a20218aba8bc
Paula Tavares <paula.tavares@editorasanar.com.br>
2019-03-12 00:55:46 -0300

    Adding jinja to generate from template

--- routes/Route.py
+++ routes/Route.py
@@ -1,6 +1,8 @@
 import io
 import webbrowser
 import os
+from jinja2 import FileSystemLoader, Environment, select_autoescape
+from config import google_api_key
 
 class Route:
     def __init__(self, latitudes, longitudes):
@@ -16,6 +18,16 @@ class Route:
         return endpoint
 
     def map(self):
+        loader = FileSystemLoader('routes')
+        env = Environment (loader=loader, autoescape=select_autoescape(['html', 'xml']))
+        template = env.get_template('route_sample.html')
+        
+        mapsUrl = "https://maps.googleapis.com/maps/api/js?key=" + google_api_key + "&callback=initMap"
+
         with io.open('routes/location.js', 'w', encoding='utf8') as location_file:
             location_file.write(unicode(self._waypoints()))
-        webbrowser.open('file://' + os.path.realpath('routes/route_sample.html'))
+
+        with io.open('routes/route.html', 'a', encoding='utf8') as route_file:
+            route_file.write(template.render(mapsUrl=mapsUrl))
+        
+        webbrowser.open('file://' + os.path.realpath('routes/route.html'))
